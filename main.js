7f496ce92208f3916c74bf0d398e5caf61786722
ishantmishra <ishant.mishra.1029@gmail.com>
2021-02-03 10:44:20 +0530

    Digital clock

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function clock ()
+{
+    let date = new Date();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    
+    let period = "AM";
+    if(hrs == 0){
+        h = 12;
+    }
+    else if (hrs >= 12){
+        hrs = hrs - 12;
+        period = "PM";
+    }
+    hrs = (hrs < 10)? "0" + hrs:hrs;
+    min = (min < 10)? "0" + min:min; 
+    sec = (sec < 10)? "0" + sec:sec;    
+
+    let time = `${hrs}:${min}:${sec}` 
+    document.getElementById("clock").innerText = time;
+    setTimeout(clock, 1000);
+}
+clock();
+
+
